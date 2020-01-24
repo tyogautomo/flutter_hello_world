e91fb41dabfbad9319b26ee886bc7232e0f4732b
tyogautomo <yoga.utomo27@gmail.com>
2020-01-24 15:50:49 +0700

    initial ocmmit

---
+++ lib/products.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Products extends StatelessWidget {
+  final List<String> products;
+
+  Products([this.products = const []]) {
+    print('[Product] constructed');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('[Product] built');
+    return Column(
+        children: products
+            .map(
+              (element) => Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/food.jpg'),
+                    Text(element)
+                  ],
+                ),
+              ),
+            )
+            .toList());
+  }
+}
